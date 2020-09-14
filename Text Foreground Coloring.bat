b3d992ac1688bf3705ca69fbfd95ac589c3cc750
Alvin Seville <alvinseville7cf@gmail.com>
2020-09-14 09:38:52 +0000

    Text ... Coloring.bat

---
+++ Text Foreground Coloring.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "colors[0]=9"
+set "colors[1]=A"
+set "colors[2]=B"
+set "colors[3]=C"
+set "colors[4]=D"
+set "colors[5]=E"
+set "colors[6]=F"
+set /a "length=7"
+
+set /a "sleep=1"
+
+set "text=Hello World"
+
+cls
+echo %text%
+
+:while_true
+set /a "i=%random% %% %length%"
+color !colors[%i%]!
+timeout %sleep% > nul
+goto :while_true
+
+setlocal disabledelayedexpansion
+@echo on
