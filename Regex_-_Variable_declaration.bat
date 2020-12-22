5edda24aa9e05f1a96f99074e25fc7e45e2b1431
AlvinSeville7cf <AlvinSeville7cf@gmail.com>
2020-12-22 04:15:18 +0000

    Regex_-_Variable_declaration.bat fixes: - missing operand error fix - error code usage introduced at global level - test_pattern output colored - code simplified

--- Regex_-_Variable_declaration.bat
+++ Regex_-_Variable_declaration.bat
@@ -1,5 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
+call :init
+
 set "alpha=abcdefghijklmnopqrstuvwxyz"
 set "digit=0123456789"
 set "alnum=%alpha%%digit%"
@@ -7,20 +9,23 @@ set "alnum=%alpha%%digit%"
 set "baseType=[%alpha%][%alnum%]*[ ][ ]*[%alpha%][%alnum%]*[ ]*=[ ]*[%alnum%][%alnum%]*"
 
 call :test_pattern "%baseType%" "int x=1" "x=1" "=1" "1" "int x=" "x=" "=" "int x" "x" "int"
+set "testPatternErrorLevel=%ERRORLEVEL%"
 
 pause
-@goto eof
+@goto :eof
 
 :init
     set /a "ec_success=0"
     set /a "ec_unknown_error=1"
+
+    call :set_esc
 
     cls
 exit /b %ec_success%
 
 :test_pattern
     set /a "ec_matching_failed=10"
-    set /a "status=%success%"
+    set /a "status=%ec_success%"
 
     set "pattern=%~1"
 
@@ -38,14 +43,21 @@ exit /b %ec_success%
     set /a "i=1"
 
     for /l %%i in (1 1 %count%) do (
-        echo !value[%%i]!| findstr /I /R /C:"^%pattern%$" > nul
-        if errorlevel 1 (
-            echo "!value[%%i]!" doesn't match pattern
+        echo !value[%%i]!| findstr /I /R /C:"^%pattern%$" > nul && (
+            echo %ESC%[32m"!value[%%i]!" matches pattern%ESC%[0m
+        ) || (
+            echo %ESC%[31m"!value[%%i]!" doesn't match pattern%ESC%[0m
             set /a "status=%ec_matching_failed%"
-        ) else (
-            echo "!value[%%i]!" matches pattern
         )
     )
 exit /b %status%
 
+:set_esc
+    for /f "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+        set "esc=%%b"
+        exit /b 0
+    )
+exit /b %ec_success%
+
 :eof
+exit /b %testPatternErrorLevel%
