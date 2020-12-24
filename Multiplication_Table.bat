ee9390bc0301d182d2629ad0ab13164e7ad4bd8c
AlvinSeville7cf <AlvinSeville7cf@gmail.com>
2020-12-24 13:43:06 +0000

    Multiplication table update: - table coloring without blinking

--- Multiplication_Table.bat
+++ Multiplication_Table.bat
@@ -1,41 +1,73 @@
 @echo off
-call :init
 setlocal enabledelayedexpansion
+call :init
+
 call :echo_table
-call :blinking
+
+pause
 @goto :eof
 
 :init
-    set "ec_success=0"
-    set "ec_unknown_error=1"
-
-    set "colors[0]=9" && set "colors[1]=A" && set "colors[2]=B"
-    set "colors[3]=C" && set "colors[4]=D" && set "colors[5]=E"
-    set "colors[6]=F" && set /a "length=7"
-
-    set /a "sleep=1"
+    set /a "ec_success=0"
+    set /a "ec_unknown_error=1"
 
     cls
+    call :set_esc
+exit /b %ec_success%
+
+:get_length
+    set "stringToGetLength=%~1"
+    set /a "i=0"
+
+    :get_char_loop
+        call set "char=%%stringToGetLength:~%i%,1%%"
+        if defined char (
+            set /a "i+=1"
+            goto :get_char_loop
+        )
+    
+    set /a "result=%i%"
+exit /b %ec_success%
+
+:pad_left
+    set "stringToPadLeft=%~1"
+    set /a "fieldWidth=%~2"
+
+    call :get_length "%stringToPadLeft%"
+    set "length=%result%"
+
+    set /a "difference=%fieldWidth% - %length%"
+    for /l %%i in (1 1 %difference%) do set "stringToPadLeft=.!stringToPadLeft!"
+
+    set "result=%stringToPadLeft%"
 exit /b %ec_success%
 
 :echo_table
     for /l %%i in (1, 1, 9) do (
         for /l %%j in (1, 1, 9) do (
             set /a "product=%%i*%%j"
-            set "result=!product! "
-            if !product! lss 10 set "result=!result! "
-            echo | set /p="!result!"
+            set /a "reminder=!product! %% 2"
+
+            call :pad_left !product! 6
+            set "product=!result!"
+            
+            if !reminder! equ 0 (
+                echo echo| set /p "=%ESC%[34m"
+            ) else (
+                echo echo| set /p "=%ESC%[91m"
+            )
+
+            echo| set /p "=!result!%ESC%[0m"
         )
         echo.
     )
 exit /b %ec_success%
 
-:blinking
-    :while_true
-    set /a "i=%random% %% %length%"
-    color !colors[%i%]!
-    timeout %sleep% > nul
-    goto :while_true
+:set_esc
+    for /f "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+        set "esc=%%b"
+        exit /b 0
+    )
 exit /b %ec_success%
 
 :eof
